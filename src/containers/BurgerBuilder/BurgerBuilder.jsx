da91db2edc61e9c2cc41130116530541e1ab365f
daanjiri <62439769+daanjiri@users.noreply.github.com>
2020-10-28 19:49:09 -0500

    se agrago un modulo y un back drop

--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
-import BuildControls from '../../components/BuildControls/BuildControls';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import classes from './BurgerBuilder.module.css';
+import Modal from '../../components/UI/Modal/Modal.jsx';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES={
     salad:0.5,
@@ -21,11 +23,24 @@ class BurgerBuilder extends Component {
            meat:0,
        },
        totalPrice:4,
+       purchasable:false,
+       purchasing:false,
+    }
+    
+    updatePurchaseState = (ingredients)=>{
+  
+        const sum = Object.keys(ingredients)
+                    .map(igKey=>ingredients[igKey])
+                    .reduce((sum,el)=>(sum+el),0)
+        console.log(sum)
+        this.setState(()=>({purchasable: sum>0}),()=>console.log(this.state))
+        
     }
 
     addIngredientHandler = (type)=>{
     
        const priceAddition = INGREDIENT_PRICES[type];
+       const updatedIngredients={...this.state.ingredients,[type]:this.state.ingredients[type]+1}
        
        /*se usa setstate con una funcion para usar el parametro prevState para que se use el estado anterior del estado,
          tambien se usa desestructuracion {...prevState.ingredients,} para copiar los elementos de esa llave del anterior estado,
@@ -33,23 +48,37 @@ class BurgerBuilder extends Component {
          por ultimo se usa un callback function para que el estado se actualize enseguida y no por batches.
        */
        this.setState((prevState)=>({
-          ingredients:  {...prevState.ingredients, [type]:prevState.ingredients[type]+1},
+          ingredients: updatedIngredients,// {...prevState.ingredients, [type]:prevState.ingredients[type]+1},
           totalPrice: prevState.totalPrice+priceAddition,
-       }),()=>console.log(this.state));
-       
+       }),()=>console.log(this.state,"addIngredient"));
+       console.log("test1")
+       this.updatePurchaseState(updatedIngredients);
+       console.log("test2")
     }
 
 
     removeIngredientHandler = (type)=>{
         const priceDeduction = INGREDIENT_PRICES[type];
         const oldCount =this.state.ingredients[type];
+        const updatedIngredients={...this.state.ingredients,[type]:this.state.ingredients[type]-1}
+
         if(oldCount<=0){
             return;
         }
        this.setState((prevState)=>({
-          ingredients:  {...prevState.ingredients, [type]:prevState.ingredients[type]-1},
-          totalPrice: prevState.totalPrice+priceDeduction,
-       }),()=>console.log(this.state));
+          ingredients: updatedIngredients,
+          totalPrice: prevState.totalPrice-priceDeduction,
+       }),()=>console.log(this.state,"remoceIngredient"));
+
+       this.updatePurchaseState(updatedIngredients);
+    }
+
+    purchaseHandler = ()=>{
+        this.setState({purchasing:true});
+    }
+
+    purchaseCancelHandler=()=>{
+        this.setState({purchasing:false})
     }
 
     render(){
@@ -62,6 +91,9 @@ class BurgerBuilder extends Component {
         }
         return(
             <Aux>
+            <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
+                <OrderSummary ingredients={this.state.ingredients}/>
+            </Modal>
             <div className={classes.BurgerBuilder}>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls 
@@ -69,6 +101,8 @@ class BurgerBuilder extends Component {
                     ingredientRemoved={this.removeIngredientHandler}
                     disabled={disabledInfo}
                     price={this.state.totalPrice}
+                    purchasable={this.state.purchasable}
+                    ordered={this.purchaseHandler}
                 />
 
             </div>
